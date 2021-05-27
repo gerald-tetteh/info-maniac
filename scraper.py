afdec59fea72d7296285402b515999252346dab1
Gerald Addo-Tetteh <geraldadt@outlook.com>
2021-05-27 12:36:34 +0100

    changed scraper file to add data to database

--- scraper.py
+++ scraper.py
@@ -1,48 +1,40 @@
 import requests
 from bs4 import BeautifulSoup
-import  lxml
-import datetime
+from datetime import datetime
 
-date = datetime.datetime.now().date()
+from models import JobItem
+from server import db
+
 
 # Jobs from sites
 jobsItems_from_jobberman=[]
 jobsItems_from_timesjobs=[]
 
 
-
-
 def scrape_from_jobberman():
-
     jobberman_url = requests.get("https://www.jobberman.com.gh/jobs?sort_by=latest").text
     soup = BeautifulSoup(jobberman_url,'lxml')
 
     jobbermab_content = soup.find_all('header', class_="search-result__header")
 
     for job in jobbermab_content:
-        
         title_ = job.h3.text
         job_type = job.find('span', class_="search-result__job-type").text
         company=job.find('div', class_="if-content-panel padding-lr-20 flex-direction-top-to-bottom--under-lg align--start--under-lg search-result__job-meta").text
         source_url=job.find('a')['href']
-        # image=""
-        date_added=date
+        image=""
 
-        jobItem = {
-            "title":title_,
-            "company":company,
-            "job_type":job_type,
-            "source_name":"Jobberman",
-            "source_url":source_url,
-            "image":"",
-            "date_added":date_added
-        }
+        job_item = JobItem(
+            title = title_.strip(),
+            company = company.strip(),
+            job_type = job_type.strip(),
+            source_name = "Jobberman",
+            source_url = source_url,
+            image = image,
+        )
 
-        jobsItems_from_jobberman.append(jobItem)
-
-
-
-    print(jobsItems_from_jobberman)
+        jobsItems_from_jobberman.append(job_item)
+    db.session.add_all(jobsItems_from_jobberman)
   
 
 def scrape_from_timesjobs():
@@ -59,28 +51,20 @@ def scrape_from_timesjobs():
         company= job.header.h3.text
         source_url= job.header.h2.a['href']
         image=""
-        date_added=date
 
-        jobItem = {
-            "title":title_.strip(),
-            "company":company.strip(),
-            "job_type":job_type.strip(),
-            "source_name":"TimesJobs",
-            "source_url":source_url,
-            "image":"",
-            "date_added":date_added
-        }
+        job_item = JobItem(
+            title = title_.strip(),
+            company = company.strip(),
+            job_type = job_type.strip(),
+            source_name = "TimesJobs",
+            source_url = source_url,
+            image = image,
+        )
+
+        jobsItems_from_timesjobs.append(job_item)
+    db.session.add_all(jobsItems_from_timesjobs)
         
-
-        jobsItems_from_timesjobs.append(jobItem)
-
-    print(jobsItems_from_timesjobs)
-        
-
-
-
-
-
-
 
 scrape_from_timesjobs()
+scrape_from_jobberman()
+db.commit()
