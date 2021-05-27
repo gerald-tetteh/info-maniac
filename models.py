ca73089cb40a5c342d64e64bcc14083a0c60d478
Gerald Addo-Tetteh <geraldadt@outlook.com>
2021-05-27 11:16:47 +0100

    chttps://github.com/login/oauth/authorize?response_type=code&client_id=0120e057bd645470c1ed&state=8fa3819af9984f64a1a6f3e6b62baca1&code_challenge_method=S256&code_challenge=fqNSxafbVUttwf7H04Uee2bjW273m4kqGJOxsuVfTDs&redirect_uri=http%3a%2f%2flocalhost%3a61389%2f&scope=repo+gist+workflo://github.com/login/oauth/authorize?response_type=code&client_id=0120e057bd645470c1ed&state=8fa3819af9984f64a1a6f3e6b62baca1&code_challenge_method=S256&code_challenge=fqNSxafbVUttwf7H04Uee2bjW273m4kqGJOxsuVfTDs&redirect_uri=http%3a%2f%2flocalhost%3a61389%2f&scope=repo+gist+workflow+changed db schema

--- models.py
+++ models.py
@@ -8,7 +8,8 @@ class JobItem(db.Model):
   source_url = db.Column(db.Text, nullable=False)
   image_url = db.Column(db.Text, nullable=False, default="default.png")
   date_added = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-  source_site_name = db.Column(db.Text, nullable=False)
+  source_name = db.Column(db.Text, nullable=False)
+  job_type = db.Column(db.Text, nullable=False, default="Unknown")
 
   def __repr__(self) -> str:
     return f"JobItem({self.title},{self.company},{self.source_site_name})"
