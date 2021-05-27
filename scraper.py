94a6ae0ed1a680451da68f41409c68864375548e
Ennin Francis <enninfrancis47@gmail.com>
2021-05-27 10:08:40 +0000

    addd scrapper

---
+++ scraper.py
@@ -0,0 +1,86 @@
+import requests
+from bs4 import BeautifulSoup
+import  lxml
+import datetime
+
+date = datetime.datetime.now().date()
+
+# Jobs from sites
+jobsItems_from_jobberman=[]
+jobsItems_from_timesjobs=[]
+
+
+
+
+def scrape_from_jobberman():
+
+    jobberman_url = requests.get("https://www.jobberman.com.gh/jobs?sort_by=latest").text
+    soup = BeautifulSoup(jobberman_url,'lxml')
+
+    jobbermab_content = soup.find_all('header', class_="search-result__header")
+
+    for job in jobbermab_content:
+        
+        title_ = job.h3.text
+        job_type = job.find('span', class_="search-result__job-type").text
+        company=job.find('div', class_="if-content-panel padding-lr-20 flex-direction-top-to-bottom--under-lg align--start--under-lg search-result__job-meta").text
+        source_url=job.find('a')['href']
+        # image=""
+        date_added=date
+
+        jobItem = {
+            "title":title_,
+            "company":company,
+            "job_type":job_type,
+            "source_name":"Jobberman",
+            "source_url":source_url,
+            "image":"",
+            "date_added":date_added
+        }
+
+        jobsItems_from_jobberman.append(jobItem)
+
+
+
+    print(jobsItems_from_jobberman)
+  
+
+def scrape_from_timesjobs():
+    timesjobs_url = "https://www.timesjobs.com/candidate/job-search.html?searchType=Home_Search&from=submit&asKey=OFF&txtKeywords=&cboPresFuncArea=35"
+    timesjobs_content = requests.get(timesjobs_url).text
+
+    soup = BeautifulSoup(timesjobs_content,'lxml')
+
+    jobs = soup.find_all('li', class_="clearfix job-bx wht-shd-bx")
+
+    for job in jobs:
+        title_ = job.header.h2.text
+        job_type = "Unknown"
+        company= job.header.h3.text
+        source_url= job.header.h2.a['href']
+        image=""
+        date_added=date
+
+        jobItem = {
+            "title":title_.strip(),
+            "company":company.strip(),
+            "job_type":job_type.strip(),
+            "source_name":"TimesJobs",
+            "source_url":source_url,
+            "image":"",
+            "date_added":date_added
+        }
+        
+
+        jobsItems_from_timesjobs.append(jobItem)
+
+    print(jobsItems_from_timesjobs)
+        
+
+
+
+
+
+
+
+scrape_from_timesjobs()
