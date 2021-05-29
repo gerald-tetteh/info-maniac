d9362b850b3072a0a1a65250e69ccabed9f8e9d7
Hilda Amadu <hildaamadu@gmail.com>
2021-05-29 15:33:05 -0700

    choice

--- info_maniac/routes.py
+++ info_maniac/routes.py
@@ -7,6 +7,19 @@ def home():
   job_items = JobItem.query.all()
   return render_template("home.html", header_text="info maniac", show_search=True, job_items=job_items)
 
+@app.route("/jobberman")
+def jobberman():
+  job_items = JobItem.query.filter_by(source_name="Jobberman").all()
+  return render_template("home.html", header_text="info maniac", show_search=True, job_items=job_items)
+
+@app.route("/times-jobs")
+def times_jobs():
+  job_items = JobItem.query.filter_by(source_name="TimesJobs").all()
+  return render_template("home.html", header_text="info maniac", show_search=True, job_items=job_items)   
+
 @app.route("/search", methods=["POST"])
 def search():
   return redirect(url_for("home"))
+
+  
+
